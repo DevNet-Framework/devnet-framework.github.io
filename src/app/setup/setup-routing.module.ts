3d5a542841641c39c10e492ed70f66725ca034fd
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-11-16 15:18:27 -0500

    add OverviewComponent to SetupModule add framework.svg to assets/images add styles to styles.css

--- src/app/setup/setup-routing.module.ts
+++ src/app/setup/setup-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'docs/setup/overview', component: OverviewComponent },
+  { path: 'docs/setup', redirectTo: 'docs/setup/overview', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/setup/setup.module.ts
+++ src/app/setup/setup.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupRoutingModule } from './setup-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OverviewComponent
+  ],
   imports: [
     CommonModule,
     SetupRoutingModule,
