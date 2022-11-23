68989397d360a887ba73e14d109100f2d8648e82
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-11-22 19:45:24 -0500

    add StartComponent to WebModule

--- src/app/web/web-routing.module.ts
+++ src/app/web/web-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'docs/web/start', component: StartComponent },
+  { path: 'docs/web', redirectTo: 'docs/web/start', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebRoutingModule } from './web-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StartComponent
+  ],
   imports: [
     CommonModule,
     WebRoutingModule,
